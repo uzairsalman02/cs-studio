87f723bdea1591721282926a56e2405a4d011ebe
Uzair Salman <uzairsalman66@gmail.com>
2026-08-23 17:45:24 +0500

    chore: clean wipe legacy components and reset architecture

--- src/app/learn/[curriculum]/[unit]/[topic]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function LearningLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="w-full h-full">{children}</div>;
-}
--- src/app/learn/[curriculum]/[unit]/[topic]/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { TopicStudio } from "@/components/studio/TopicStudio";
-
-export default function LearningTopicPage() {
-  return <TopicStudio />;
-}
